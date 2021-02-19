7ee594ee07085f56fd9c5b51450757dbabf8b245
Antoine J.C. Zink <77839973+Zink-Antoine@users.noreply.github.com>
2021-02-19 16:24:38 +0100

    Add files via upload

--- R/Lum.R
+++ R/Lum.R
@@ -1,6 +1,33 @@
-############################# fonction Lum ####################################
+#' Lum
+#' extracts TL measurements
+#'
+#' @param file [Risoe.BINfileData-class] (**required**) the BIN/BINX file
+#' @param ech [numeric] (**with default**) the sample number
+#' @param Doseb0 [numeric] (**with default**)  the reference of beta irradiation (in seconds)
+#' @param Dosea0 [numeric] (**with default**)  the reference of alpha irradiation (in seconds)
+#' @param alpha [logical] (**with default**) TRUE if alpha measurements are inclued. Based on the number of data files, the value is corrected
+#' @param supra [logical] (**with default**) TRUE if supra measurements are inclued. Based on the number of data files, the value is corrected
+#' @param TypLum [character] (*with default**) luminescence type "TL" = signal; "BG" = background
+#' @param Temp [numerical vector] (**with default**) the temperature range
+#'
+#' @return a list
+#' @return $alpha
+#' @return $supra
+#' @return $b glowcurve corrected from background for beta irradiation
+#' @return $a glowcurve corrected from background for alpha irradiation
+#' @return $n glowcurve corrected from background for normalisation
+#' @return $Bb uncorrected glowcurve for beta irradiation
+#' @return $Ba uncorrected glowcurve for alpha irradiation
+#' @return $Bn uncorrected glowcurve for normalisation
+#' @return $bsup glowcurve corrected from background for supralinearity irradiation
+#' @return $nsup glowcurve corrected from background for supralinearity nomalisation
+#' @return $Bbsup uncorrected glowcurve for supralinearity irradiation
+#' @return $Bnsup uncorrected glowcurve for supralinearity nomalisation
+#' @return Temp.corr correction of the monitored temperature for Lexsyg readers
+#'
+#'
 `Lum` <-
-function(file,ech=1,Doseb0=90,Dosea0=90,alpha=TRUE,supra=TRUE,TypLum=c("TL","BG"),Temp=seq(26,599)) #détermination la table des valeurs normalisées des OSL ech= numéro échantillon, OSL= type OSL
+function(file,ech=1,Doseb0=90,Dosea0=90,alpha=TRUE,supra=TRUE,TypLum=c("TL","BG"),Temp=seq(26,599))
 	{
 
 	D1<-Raw.Data(file,ech,1)
@@ -12,17 +39,16 @@ function(file,ech=1,Doseb0=90,Dosea0=90,alpha=TRUE,supra=TRUE,TypLum=c("TL","BG"
 	if(file[[1]]@METADATA$SYSTEMID[1]==0)Temp.corr<-Temp-file[[1]]@METADATA$LOW[1]
 	if(file[[1]]@METADATA$SYSTEMID[1]==88)Temp.corr<-Temp
 
-	B1<-array(unlist(D1$Brut),dim=c(D1$T,D1$L))#pur matrice pour calcul
-
+	B1<-array(unlist(D1$Brut),dim=c(D1$T,D1$L))#pur matrix to calculate
 	Net1<-B1[,D1$TL]-B1[,D1$BG]
 	Net1[Net1<0]<-0
 
-	#pas de préchauffe enregistrée
+	#no preheat
 	cycle0<-2 #TL+BG
-	nbd<-9+alpha*4 #9 disques, 13 si alpha
-	cycleTLb<-cycle0*3*3 #(TL,BG)*{nat,nat+b,nat+2*b}*3 
-	if (alpha)cycleTLa<-cycle0*2*2 #(TL,BG)*2*{nat+a,nat+2*a} 
-	cycleTLn<-cycle0*nbd #(TL,BG)*13 disques
+	nbd<-9+alpha*4 #9 discs (with alpha, 13)
+	cycleTLb<-cycle0*3*3 #(TL,BG)*{nat,nat+b,nat+2*b}*3
+	if (alpha)cycleTLa<-cycle0*2*2 #(TL,BG)*2*{nat+a,nat+2*a}
+	cycleTLn<-cycle0*nbd #(TL,BG)*nbd discs
 
 	Dosen<-c(Doseb0)
 	Doseb<-c(0,Doseb0,2*Doseb0)
@@ -53,15 +79,15 @@ function(file,ech=1,Doseb0=90,Dosea0=90,alpha=TRUE,supra=TRUE,TypLum=c("TL","BG"
 		if(file[[2]]@METADATA$SYSTEMID[1]==0)Temp.corr<-Temp-file[[2]]@METADATA$LOW[1]
 		if(file[[2]]@METADATA$SYSTEMID[1]==88)Temp.corr<-Temp
 
-		B2<-array(unlist(D2$Brut),dim=c(D2$T,D2$L))#pur matrice pour calcul
+		B2<-array(unlist(D2$Brut),dim=c(D2$T,D2$L))#matrix need to calculate
 
 		Net2<-B2[,D2$TL]-B2[,D2$BG]
 		Net2[Net2<0]<-0
 
-		#pas de préchauffe enregistrée
+		#no monitored preheat
 		cycle0<-2 #TL+BG
-		nbd2<-9 #9 disques
-		cycleTL<-cycle0*3*3 #(TL,BG)*{b,2*b,3*b}*3 
+		nbd2<-9 #9 discs
+		cycleTL<-cycle0*3*3 #(TL,BG)*{b,2*b,3*b}*3
 
 		Doseb2<-c(Doseb0,2*Doseb0,3*Doseb0)
 
@@ -83,28 +109,29 @@ function(file,ech=1,Doseb0=90,Dosea0=90,alpha=TRUE,supra=TRUE,TypLum=c("TL","BG"
 	}
 
 ##########################
+
 Raw.Data<-function(file,ech,n.chauf) {
 	file.sel<-RW.File(file,n.chauf,file[[n.chauf]]@METADATA$SEL)
-	L<-length(file.sel)  #nombre de mesures
+	L<-length(file.sel)   #number of measures
 	corr<-L%%36
-	L<-L-corr  #nombre de mesures corrigé des préchauffes du four
-	T<-file.sel@METADATA$NPOINT[1] #nombre de point de mesure (1pt par degré)
-	if (L==72)	L<-36#deux échantillons
-	Brut<-file.sel@DATA[seq(1,L)+corr] #données brutes issue de la chauffe n° n.chauf après élimination des préchauffes du four
-	if (ech==2){		
+	L<-L-corr   #number of measurements corrected for furnace preheating
+	T<-file.sel@METADATA$NPOINT[1]  # number of measuring points (1 point per degree)
+	if (L==72)	L<-36#two samples
+	Brut<-file.sel@DATA[seq(1,L)+corr]  #Raw data from heating n# n.chauf after removal of the furnace preheating
+	if (ech==2){
 			Brut<-file.sel@DATA[seq(L,2*L)+corr]
 			}
-	TL<-seq(1,L-1,2) # numérotation des TL
-	BG<-seq(2,L,2) #numérotation des BF
+	TL<-seq(1,L-1,2) # TL numbering
+	BG<-seq(2,L,2) # BG numbering
 
-	RD<-list(Brut=Brut,L=L,T=T,TL=TL,BG=BG)	
+	RD<-list(Brut=Brut,L=L,T=T,TL=TL,BG=BG)
 	return(RD)
 }
 
 
-#############reconstitution#########################
-#reconstitue un fichier théorique à partir 
-#d’un fichier contenant une séquence inusuelle
+#############Rw.file function (internal)#########################
+#reconstructs a theoretical file to start from
+#a file containing an unusual sequence
 
 RW.File<-function(file,nfile,sequence){
 file.n<-file[[nfile]]
--- R/Step.Out.R
+++ R/Step.Out.R
@@ -1,9 +1,25 @@
 ############Stepping out################
+#' Step.Out
+#'
+#' Slice sampler: stepping out procedure
+#' Neal(2003) fig.3
+#'
+#' @param foo [function] (**required**) function proportional to the density
+#' @param x0 [numeric] (**required**) the current point
+#' @param y [numeric] (**required**) the vertical level defining the slice
+#' @param w [numeric] (**required**) estimate of the typical size of a slice
+#' @param m [numeric] (**required**) integer limiting the size of a slice to mw
+#' @param Rmx [numeric] (**required**) bound value to limit edge effects (added)
+#'
+#' @import stats
+#'
+#' @return
+#' @export
+#'
+#'
 Step.Out<-function(foo,x0,y,w,m,Rmx=500){
-#stepping- out procedure
-#Neal (2003) fig.3
 
-	if(y>foo(x0)) return(print("x0 non valable"))
+	if(y>foo(x0)) return(print("x0 invalid"))
 
 	U<-runif(1,0,1)
 	L<-x0-w*U
@@ -22,7 +38,7 @@ Step.Out<-function(foo,x0,y,w,m,Rmx=500){
 	while(K>0 && y<foo(R)&& R<Rmx){
 		R<-R+w
 		K<-K-1
-		if (R>Rmx) {R<-Rmx}#ligne de contrôle rajouté effet de bord
+		if (R>Rmx) {R<-Rmx} #control line added to limit edge effects
 		}
 
 	return(c(L,R,J,K))
