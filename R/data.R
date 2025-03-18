95d9f7d26a6d129705fa3f225dd9b37abeacb1da
Zink <antione.zink@culture.gouv.fr>
2025-03-18 13:50:19 +0100

    test

--- R/data.R
+++ R/data.R
@@ -2,7 +2,7 @@
 #'
 #' TL example from RLumModel
 #' a dataset containing multiples TL calculated with RLumModel
-#' call function "model_LuminescenceSignals", model = "Bailey2001"
+#' call function "model_LuminescenceSignals", model = "Bailey2001" test
 #' simulate_sample_history = TRUE
 #'
 #' @format A list with 4 variables: \code{Dose}, \code{df.T},
